928cf5325180470edfe65c414a4f7cf4e5a0150c
Jonathan Lam <jlam55555@gmail.com>
2022-02-06 18:17:55 -0500

    Add some to introduction, add pygmentize lexer for Hazel code in minted environments

---
+++ latex-includes/hazel_lexer.py
@@ -0,0 +1,35 @@
+from pygments.lexer import RegexLexer, words
+from pygments.token import *
+
+class HazelLexer(RegexLexer):
+    """
+    Very simple lexer for the Hazel experimental structured programming language.
+
+    Structure based off of the Go language lexer.
+    https://github.com/pygments/pygments/blob/master/pygments/lexers/go.py
+    """
+    
+    name = 'Hazel'
+    aliases = ['hazel']
+    filenames = ['*.hz']
+
+    tokens = {
+        'root': [
+            # whitespace
+            (r'\n', Whitespace),
+            (r'\s+', Whitespace),
+            # keywords
+            (words(('let', 'in', 'of', 'case'), suffix=r'\b'), Keyword),
+            (r'inj\[R|L\]', Keyword),
+            # literals
+            (r'(true|false|\[\])\b', Keyword.Constant),
+            (r'\d+', Number),
+            (r'\d+\.\d*', Number),
+            (r'\d*\.\d+', Number),
+            # Tokens
+            (r'[+\-*/<>=]|<|>|==|\+.|\-.|\*.|\/.|<.|>.|==.|::', Operator),
+            (r'[.,<>(){}]', Punctuation),
+            # identifier
+            (r'[^\W\d]\w*', Name.Other),
+        ]
+    }
